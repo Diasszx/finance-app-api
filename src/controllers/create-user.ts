a89b53632a852660785c425d96d4b127b44aedc4
Adam <adamrpdiass@hotmail.com>
2026-07-27 19:09:08 -0300

    fix: remove unecessary await

--- src/controllers/create-user.ts
+++ src/controllers/create-user.ts
@@ -1,7 +1,26 @@
+import type { Request, Response } from "express";
+import { createUserSchema } from "../schemas/users/create-user.schema.js";
+import { CreateUserService } from "../services/create-user.js";
+import { ZodError } from "zod";
+
 export class CreateUserController {
-  async execute() {
-    // validar a requisicao (campos obrigatorios, tamanho de senha e email)
-    // chamar o service
-    // retornar a reposta para o usuario ( status code)
+  async execute(req: Request, res: Response) {
+    try {
+      const params = req.body;
+      const body = createUserSchema.parse(params);
+      const service = new CreateUserService();
+      const user = await service.execute(body);
+      return res.status(201).json(user);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res.status(400).json({
+          message: "Dados inválidos",
+          errors: error.issues,
+        });
+      }
+      return res.status(500).json({
+        message: "Erro interno do servidor.",
+      });
+    }
   }
 }
