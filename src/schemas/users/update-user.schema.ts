84f064d77ec2678c5d665d1b84e4bd23991cdf2e
Adam <adamrpdiass@hotmail.com>
2026-07-29 16:53:31 -0300

    feat: add updateUserSchema for user update validation

---
+++ src/schemas/users/update-user.schema.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const updateUserSchema = z.object({
+  firstName: z.string().min(2, "O primeiro nome deve ter pelo menos 2 caracteres.").optional(),
+  lastName: z.string().min(2, "O sobrenome deve ter pelo menos 2 caracteres.").optional(),
+  email: z.email("O e-mail informado é inválido.").optional(),
+  password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres.").optional(),
+});
+
+export type UpdateUserDTO = z.infer<typeof updateUserSchema>;
