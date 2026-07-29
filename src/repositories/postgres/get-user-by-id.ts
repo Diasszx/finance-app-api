33cd9db19165a7eb9e16638bfef80b0c8a413c8a
Adam <adamrpdiass@hotmail.com>
2026-07-29 10:00:27 -0300

    feat: update execute method to return undefined for non-existent users

--- src/repositories/postgres/get-user-by-id.ts
+++ src/repositories/postgres/get-user-by-id.ts
@@ -2,14 +2,10 @@ import { PostgresHelper } from "../../db/postgres/helper.js";
 import type { User } from "../../entities/user.entity.js";
 
 export class PostgresGetUserByIdRepository {
-  async execute(userId: string): Promise<User> {
+  async execute(userId: string): Promise<User | undefined> {
     const users = await PostgresHelper.query<User>("SELECT * FROM users where id = $1", [userId]);
 
     const [user] = users;
-
-    if (!user) {
-      throw new Error("Usuário não encontrado.");
-    }
 
     return user;
   }
