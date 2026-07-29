229bd16d95d7f8740a051046ed03e479bd76c74b
Adam <adamrpdiass@hotmail.com>
2026-07-29 16:26:38 -0300

    feat: implement UpdateUserService for user update functionality

---
+++ src/services/update-users.ts
@@ -0,0 +1,32 @@
+import { EmailAlreadyInUseError } from "../erros/user.js";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { PostgresUpdateUserRepository } from "../repositories/postgres/update-user.js";
+import type { UpdateUserDTO } from "../schemas/update-user.schema.js";
+import bcrypt from "bcrypt";
+
+export class UpdateUserService {
+  async execute(userId: string, updateUsers: UpdateUserDTO) {
+    if (updateUsers.email) {
+      const postgresGetUserByEmailRepository = new PostgresGetUserByEmailRepository();
+
+      const userWithProvidedEmail = await postgresGetUserByEmailRepository.execute(
+        updateUsers.email,
+      );
+      if (userWithProvidedEmail) {
+        throw new EmailAlreadyInUseError(updateUsers.email);
+      }
+    }
+    const user = {
+      ...updateUsers,
+    };
+    if (updateUsers.password) {
+      const hashedPassword = await bcrypt.hash(updateUsers.password, 10);
+      user.password = hashedPassword;
+    }
+
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+    const updatedUser = await postgresUpdateUserRepository.execute(userId, user);
+
+    return updatedUser;
+  }
+}
