78745670a9b09a8cdd40837bba03b2f4acc8b621
Adam <adamrpdiass@hotmail.com>
2026-07-31 16:38:54 -0300

    feat: restructure user services and move to user directory

--- src/services/user/create-user.ts
+++ src/services/user/create-user.ts
@@ -1,11 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
-import type { CreateUserDTO } from "../schemas/users/create-user.schema.js";
-import type { User } from "../entities/user.entity.js";
-import { EmailAlreadyInUseError } from "../erros/user.js";
-import type { GetUserByEmailRepositoryInterface } from "../repositories/interfaces/user/get-user-by-email.js";
-import type { CreateUserRepositoryInterface } from "../repositories/interfaces/user/create-user.js";
-import type { CreateUserServiceInterface } from "./interfaces/create-user.js";
+import type { CreateUserDTO } from "../../schemas/users/create-user.schema.js";
+import type { User } from "../../entities/user.entity.js";
+import { EmailAlreadyInUseError } from "../../erros/user.js";
+import type { GetUserByEmailRepositoryInterface } from "../../repositories/interfaces/user/get-user-by-email.js";
+import type { CreateUserRepositoryInterface } from "../../repositories/interfaces/user/create-user.js";
+import type { CreateUserServiceInterface } from "../interfaces/create-user.js";
 export class CreateUserService implements CreateUserServiceInterface {
   constructor(
     private readonly createUserRepository: CreateUserRepositoryInterface,
