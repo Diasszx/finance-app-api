29c7ae902a331029d944f7c903b2768579f2b86b
Adam <adamrpdiass@hotmail.com>
2026-07-31 15:18:27 -0300

    feat: add index files for controllers, repositories, and services to streamline exports

---
+++ src/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from "./postgres/create-user.js";
+export * from "./postgres/delete-user.js";
+export * from "./postgres/get-user-by-id.js";
+export * from "./postgres/get-user-by-email.js";
+export * from "./postgres/update-user.js";
