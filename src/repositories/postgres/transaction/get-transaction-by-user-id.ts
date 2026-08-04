d3538ab67953fbf99f63cc18ca4f54c2a37406e6
Adam <adamrpdiass@hotmail.com>
2026-08-04 10:49:38 -0300

    feat: rename interface for retrieving transactions by user ID to improve clarity

--- src/repositories/postgres/transaction/get-transaction-by-user-id.ts
+++ src/repositories/postgres/transaction/get-transaction-by-user-id.ts
@@ -1,8 +1,8 @@
 import { PostgresHelper } from "../../../db/postgres/helper.js";
 import type { Transaction } from "../../../entities/transaction.entity.js";
-import type { GetTransactionByUserId } from "../../interfaces/transaction/get-transaction-by-user-id.js";
+import type { GetTransactionByUserIdInterface } from "../../interfaces/transaction/get-transaction-by-user-id.js";
 
-export class PostgresGetTransactionByUserId implements GetTransactionByUserId {
+export class PostgresGetTransactionByUserIdRepository implements GetTransactionByUserIdInterface {
   async execute(userId: string): Promise<Transaction[]> {
     const transactions = await PostgresHelper.query<Transaction>(
       `
