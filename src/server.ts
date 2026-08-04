d950fb6a0d48021905e9f3e9efd353305e34dd30
Adam <adamrpdiass@hotmail.com>
2026-08-04 01:34:19 -0300

    feat: add Swagger UI integration for API documentation

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,9 @@ import {
   makeUpdateUserController,
 } from "./factories/users.js";
 import { makeCreateTransactionController } from "./factories/controllers/transaction.js";
+import fs from "fs";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 app.use(express.json());
@@ -46,3 +49,9 @@ app.post("/api/transactions/:userId", async (req: Request, res: Response) => {
 });
 
 app.listen(process.env.PORT, () => console.log("Listeing port 8080"));
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(import.meta.dirname, "docs/swagger.json"), "utf8"),
+);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
