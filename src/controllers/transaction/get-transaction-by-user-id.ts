c744660aaf5f28a88ffd3f815372f7f437ab75ff
Adam <adamrpdiass@hotmail.com>
2026-08-04 11:49:49 -0300

    feat: enhance GetTransactionsByUserIdController to return transactions and improve error handling

--- src/controllers/transaction/get-transaction-by-user-id.ts
+++ src/controllers/transaction/get-transaction-by-user-id.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { internalServerError, notFound } from "../utils/http-response.js";
+import { internalServerError, notFound, ok } from "../utils/http-response.js";
 import { UserNotFoundError } from "../../erros/userId.js";
 import type { GetTransactionByIdServiceInterface } from "../../services/interfaces/transaction/get-transaction-by-user-id.js";
 import {
@@ -9,12 +9,14 @@ import {
 import type { TypedRequestQuery } from "../utils/http.js";
 
 export class GetTransactionsByUserIdController {
-  constructor(private readonly getTransactionByUserId: GetTransactionByIdServiceInterface) {}
+  constructor(
+    private readonly getTransactionsByUserByidService: GetTransactionByIdServiceInterface,
+  ) {}
   async execute(req: TypedRequestQuery<GetTransactionByIdQueryDTO>, res: Response) {
     try {
-      const { userId } = req.query;
-      const userIdIsValid = getTransactionByUserIdQuerySchema.parse(userId);
-      const user = this.getTransactionByUserId.execute(userId);
+      const { userId } = getTransactionByUserIdQuerySchema.parse(req.query.userId);
+      const transactions = await this.getTransactionsByUserByidService.execute(userId);
+      return ok(res, transactions);
     } catch (error) {
       console.log(error);
       if (error instanceof UserNotFoundError) {
