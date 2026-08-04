e1e0334ecd70f092d0ebb5a68e28c8dba5618eaf
Adam <adamrpdiass@hotmail.com>
2026-08-04 10:49:07 -0300

    feat: ensure GetTransactionByUserId throws UserNotFindError

--- src/services/user/get-user-by-id.ts
+++ src/services/user/get-user-by-id.ts
@@ -1,4 +1,5 @@
 import type { User } from "../../entities/user.entity.js";
+import { UserNotFoundError } from "../../erros/userId.js";
 import type { GetUserByIdRepositoryInterface } from "../../repositories/interfaces/user/get-user-by-id.js";
 import type { getUserByIdServiceInterface } from "../interfaces/user/get-user-by-id.js";
 
@@ -6,7 +7,9 @@ export class GetUserByIdService implements getUserByIdServiceInterface {
   constructor(private readonly getUserByIdRepository: GetUserByIdRepositoryInterface) {}
   async execute(userId: string): Promise<User | null> {
     const user = await this.getUserByIdRepository.execute(userId);
-
+    if (!user) {
+      throw new UserNotFoundError(userId);
+    }
     return user;
   }
 }
