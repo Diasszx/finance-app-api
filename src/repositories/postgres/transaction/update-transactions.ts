7d36372f3ff3912d3a2f619853b58ad342e5ae50
Adam <adamrpdiass@hotmail.com>
2026-08-06 09:42:43 -0300

    feat: implement UpdateTransactionsRepository for updating transactions

---
+++ src/repositories/postgres/transaction/update-transactions.ts
@@ -0,0 +1,29 @@
+import { PostgresHelper } from "../../../db/postgres/helper.js";
+import type { Transaction } from "../../../entities/transaction.entity.js";
+import type { UpdateTransactionRepositoryInterface } from "../../interfaces/transaction/update-transaction.js";
+
+export class UpdateTransactionsRepository implements UpdateTransactionRepositoryInterface {
+  async execute(userId: string, updateTransaction: Transaction): Promise<Transaction> {
+    const updateFields: string[] = [];
+    const updateValues: unknown[] = [];
+    Object.keys(updateTransaction).forEach((key) => {
+      const typedKey = key as keyof Transaction;
+
+      updateFields.push(`${typedKey} = $${updateValues.length + 1}`);
+      updateValues.push(updateTransaction[typedKey]);
+    });
+    updateValues.push(userId);
+    const updateQuery = `
+        UPDATE transaction
+        SET ${updateFields.join(", ")}
+        WHERE id = $${updateValues.length}
+        RETURNING *
+    `;
+    const updatedTransaction = await PostgresHelper.query<Transaction>(updateQuery, updateValues);
+    const [transaction] = updatedTransaction;
+    if (!transaction) {
+      throw new Error("Falha ao atualizar usuário.");
+    }
+    return transaction;
+  }
+}
