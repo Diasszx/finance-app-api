c9fa83cfdf47e7b12288e8bae4b84f4ed962ddc3
Adam <adamrpdiass@hotmail.com>
2026-08-07 10:45:35 -0300

    refactor: simplify balance retrieval by using stored procedure

--- src/repositories/postgres/user/get-user-balance.ts
+++ src/repositories/postgres/user/get-user-balance.ts
@@ -4,20 +4,9 @@ import type { GetUserBalanceInterface } from "../../interfaces/user/get-user-bal
 
 export class PostgresGetUserBalanceRepository implements GetUserBalanceInterface {
   async execute(userId: string): Promise<Balance> {
-    const [balance] = await PostgresHelper.query<Balance>(
-      `SELECT 
-            SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
-            SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
-            SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
-            (
-                SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
-                - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
-                - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
-            ) AS balance
-            FROM transaction
-            WHERE user_id = $1`,
-      [userId],
-    );
+    const [balance] = await PostgresHelper.query<Balance>(`SELECT * FROM get_user_balance($1)`, [
+      userId,
+    ]);
     if (!balance) {
       throw new Error("Balance not found");
     }
