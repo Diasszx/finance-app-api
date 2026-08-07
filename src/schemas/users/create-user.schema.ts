734cec72d2901bfb4852401e300a8d56d3534f00
Adam <adamrpdiass@hotmail.com>
2026-08-07 15:17:56 -0300

    refactor: add trimming to user schema fields for consistency

--- src/schemas/users/create-user.schema.ts
+++ src/schemas/users/create-user.schema.ts
@@ -3,16 +3,19 @@ import { z } from "zod";
 export const createUserSchema = z.object({
   firstName: z
     .string({ error: "O primeiro nome é obrigatório." })
+    .trim()
     .min(2, "O primeiro nome deve ter pelo menos 2 caracteres."),
 
   lastName: z
     .string({ error: "O sobrenome é obrigatório." })
+    .trim()
     .min(2, "O sobrenome deve ter pelo menos 2 caracteres."),
 
-  email: z.email("O e-mail informado é inválido."),
+  email: z.email("O e-mail informado é inválido.").trim(),
 
   password: z
     .string({ error: "A senha é obrigatória." })
+    .trim()
     .min(8, "A senha deve ter no mínimo 8 caracteres."),
 });
 
--- src/schemas/users/update-user.schema.ts
+++ src/schemas/users/update-user.schema.ts
@@ -2,10 +2,14 @@ import z from "zod";
 
 export const updateUserSchema = z
   .object({
-    firstName: z.string().min(2, "O primeiro nome deve ter pelo menos 2 caracteres.").optional(),
-    lastName: z.string().min(2, "O sobrenome deve ter pelo menos 2 caracteres.").optional(),
-    email: z.email("O e-mail informado é inválido.").optional(),
-    password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres.").optional(),
+    firstName: z
+      .string()
+      .min(2, "O primeiro nome deve ter pelo menos 2 caracteres.")
+      .trim()
+      .optional(),
+    lastName: z.string().min(2, "O sobrenome deve ter pelo menos 2 caracteres.").trim().optional(),
+    email: z.email("O e-mail informado é inválido.").trim().optional(),
+    password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres.").trim().optional(),
   })
   .strict();
 
