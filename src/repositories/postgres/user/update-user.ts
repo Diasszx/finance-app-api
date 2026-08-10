c81d3c7e16935a54568216a87e932ec337025ecc
Adam <adamrpdiass@hotmail.com>
2026-08-10 14:06:14 -0300

    refactor: simplify user update logic by using utility function to strip undefined properties

--- src/repositories/postgres/user/update-user.ts
+++ src/repositories/postgres/user/update-user.ts
@@ -2,27 +2,16 @@ import type { UpdateUserDTO } from "../../../schemas/users/update-user.schema.js
 import type { User } from "../../../entities/user.entity.js";
 import type { UpdateUserRepositoryInterface } from "../../interfaces/user/update-user.js";
 import { prisma } from "../../../../prisma/prisma.js";
+import { stripUndefinedProperties } from "../../../utils/strip-undefined-properties.js";
 
 export class PostgresUpdateUserRepository implements UpdateUserRepositoryInterface {
   async execute(userId: string, updateUserParams: UpdateUserDTO): Promise<User> {
+    const updateUserData = stripUndefinedProperties(updateUserParams);
     const user = await prisma.user.update({
       where: {
         id: userId,
       },
-      data: {
-        ...(updateUserParams.firstName !== undefined && {
-          firstName: updateUserParams.firstName,
-        }),
-        ...(updateUserParams.lastName !== undefined && {
-          lastName: updateUserParams.lastName,
-        }),
-        ...(updateUserParams.email !== undefined && {
-          email: updateUserParams.email,
-        }),
-        ...(updateUserParams.password !== undefined && {
-          password: updateUserParams.password,
-        }),
-      },
+      data: updateUserData,
     });
 
     return user;
