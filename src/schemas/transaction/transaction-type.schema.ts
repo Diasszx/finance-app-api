17d2192ad8a6babaa10ef93f78caec6294f4d5a3
Adam <adamrpdiass@hotmail.com>
2026-08-10 19:43:25 -0300

    refactor: remove transaction type schema as it is no longer needed

--- src/schemas/transaction/transaction-type.schema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const transactionTypeSchema = z.enum(["EARNING", "EXPENSE", "INVESTMENT"]);
-
-export type TransactionType = z.infer<typeof transactionTypeSchema>;
