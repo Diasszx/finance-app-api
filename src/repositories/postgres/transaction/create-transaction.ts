fb955315fcb7917ef6a0179e5047c2c6536fc8bf
Adam <adamrpdiass@hotmail.com>
2026-08-10 19:43:07 -0300

    refactor: replace PostgresHelper with Prisma for creating transactions

--- src/repositories/postgres/transaction/create-transaction.ts
+++ src/repositories/postgres/transaction/create-transaction.ts
@@ -1,28 +1,20 @@
-import { PostgresHelper } from "../../../db/postgres/helper.js";
+import { prisma } from "../../../../prisma/prisma.js";
 import type { Transaction } from "../../../entities/transaction.entity.js";
 import type { CreateTransactionRepositoryInterface } from "../../interfaces/transaction/create-transaction.js";
 
 export class PostgresCreateTransactionRepository implements CreateTransactionRepositoryInterface {
-  async execute(transaction: Transaction): Promise<Transaction> {
-    const result = await PostgresHelper.query<Transaction>(
-      `
-      INSERT INTO transaction(ID, user_id, name, date, amount, type) 
-      VALUES ($1, $2, $3, $4, $5, $6) 
-      RETURNING *;`,
-      [
-        transaction.id,
-        transaction.userId,
-        transaction.title,
-        transaction.date,
-        transaction.amount,
-        transaction.type,
-      ],
-    );
-    const [createdTransaction] = result;
-    if (!createdTransaction) {
-      throw new Error("Falha ao criar transação.");
-    }
+  async execute(createTransactionParams: Transaction): Promise<Transaction> {
+    const transaction = await prisma.transaction.create({
+      data: {
+        id: createTransactionParams.id,
+        userId: createTransactionParams.userId,
+        title: createTransactionParams.title,
+        date: createTransactionParams.date,
+        amount: createTransactionParams.amount,
+        type: createTransactionParams.type,
+      },
+    });
 
-    return createdTransaction;
+    return transaction;
   }
 }
