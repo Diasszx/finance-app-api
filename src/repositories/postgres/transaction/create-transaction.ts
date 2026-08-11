504d4f65074a2c0a0dc5e561c3c16eeb28a7f20e
Adam <adamrpdiass@hotmail.com>
2026-08-10 23:13:11 -0300

    fix: ensure date is correctly formatted when creating a transaction

--- src/repositories/postgres/transaction/create-transaction.ts
+++ src/repositories/postgres/transaction/create-transaction.ts
@@ -7,6 +7,7 @@ export class PostgresCreateTransactionRepository implements CreateTransactionRep
     const transaction = await prisma.transaction.create({
       data: {
         ...createTransactionParams,
+        date: new Date(createTransactionParams.date),
       },
     });
 
