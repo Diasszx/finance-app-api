9b3aaf49467a19b8cc2fd06f5c8c35ff14b44e22
Adam <adamrpdiass@hotmail.com>
2026-08-10 22:54:22 -0300

    feat: refactor PostgresGetTransactionByUserIdRepository to use Prisma for transaction retrieval

--- src/repositories/postgres/transaction/get-transaction-by-user-id.ts
+++ src/repositories/postgres/transaction/get-transaction-by-user-id.ts
@@ -1,16 +1,17 @@
-import { PostgresHelper } from "../../../db/postgres/helper.js";
+import { prisma } from "../../../../prisma/prisma.js";
 import type { Transaction } from "../../../entities/transaction.entity.js";
 import type { GetTransactionByUserIdInterface } from "../../interfaces/transaction/get-transaction-by-user-id.js";
 
 export class PostgresGetTransactionByUserIdRepository implements GetTransactionByUserIdInterface {
   async execute(userId: string): Promise<Transaction[]> {
-    const transactions = await PostgresHelper.query<Transaction>(
-      `
-        SELECT * FROM "transaction" where user_id = $1;
-        `,
-      [userId],
-    );
-
-    return transactions ?? null;
+    const transactions = await prisma.transaction.findMany({ where: { userId } });
+    return transactions.map((transaction) => ({
+      id: transaction.id,
+      userId: transaction.userId,
+      title: transaction.title,
+      date: transaction.date.toISOString(),
+      amount: transaction.amount.toNumber(),
+      type: transaction.type,
+    }));
   }
 }
