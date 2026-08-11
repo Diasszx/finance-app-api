9a1b253a05de68140193ac426405d8eeb13e9cc1
Adam <adamrpdiass@hotmail.com>
2026-08-10 23:02:13 -0300

    refactor: simplify transaction update logic by utilizing Prisma for database interactions

--- src/repositories/postgres/transaction/update-transactions.ts
+++ src/repositories/postgres/transaction/update-transactions.ts
@@ -1,47 +1,23 @@
-import { PostgresHelper } from "../../../db/postgres/helper.js";
+import { prisma } from "../../../../prisma/prisma.js";
 import type { Transaction } from "../../../entities/transaction.entity.js";
 import type { UpdateTransactionDTO } from "../../../schemas/transaction/update-transaction.schema.js";
+import { stripUndefinedProperties } from "../../../utils/strip-undefined-properties.js";
 import type { UpdateTransactionRepositoryInterface } from "../../interfaces/transaction/update-transaction.js";
 
 export class PostgresUpdateTransactionsRepository implements UpdateTransactionRepositoryInterface {
   async execute(
     transactionId: string,
-    updateTransaction: UpdateTransactionDTO,
+    updateTransactionParams: UpdateTransactionDTO,
   ): Promise<Transaction> {
-    const updateFields: string[] = [];
-    const updateValues: unknown[] = [];
-    const fieldMap: Record<keyof UpdateTransactionDTO, string> = {
-      title: "name",
-      date: "date",
-      amount: "amount",
-      type: "type",
+    const updateTransaction = stripUndefinedProperties(updateTransactionParams);
+    const transaction = await prisma.transaction.update({
+      where: { id: transactionId },
+      data: updateTransaction,
+    });
+    return {
+      ...transaction,
+      date: transaction.date.toISOString(),
+      amount: transaction.amount.toNumber(),
     };
-    for (const [key, value] of Object.entries(updateTransaction) as [
-      keyof UpdateTransactionDTO,
-      UpdateTransactionDTO[keyof UpdateTransactionDTO],
-    ][]) {
-      if (value === undefined) continue;
-
-      const dbField = fieldMap[key];
-
-      updateFields.push(`${dbField} = $${updateValues.length + 1}`);
-      updateValues.push(value);
-    }
-    if (updateFields.length === 0) {
-      throw new Error("Nenhum campo para atualizar.");
-    }
-    updateValues.push(transactionId);
-    const updateQuery = `
-    UPDATE transaction
-    SET ${updateFields.join(", ")}
-    WHERE id = $${updateValues.length}
-    RETURNING *
-  `;
-    const updatedTransaction = await PostgresHelper.query<Transaction>(updateQuery, updateValues);
-    const [transaction] = updatedTransaction;
-    if (!transaction) {
-      throw new Error("Falha ao atualizar usuário.");
-    }
-    return transaction;
   }
 }
