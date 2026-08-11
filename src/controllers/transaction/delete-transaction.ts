bad8fca010139833389973c9bf187b9b155c7aaf
Adam <adamrpdiass@hotmail.com>
2026-08-10 22:41:44 -0300

    feat: add DeleteTransactionController for handling transaction deletion requests

---
+++ src/controllers/transaction/delete-transaction.ts
@@ -0,0 +1,27 @@
+import type { Request, Response } from "express";
+import type { DeleteTransactionServiceInterface } from "../../services/interfaces/transaction/delete-transaction.js";
+import {
+  getTransactionByIdSchema,
+  type GetTransactionByIdParamsDTO,
+} from "../../schemas/transaction/get-transaction-by-id.schema.js";
+import { badRequest, internalServerError, notFound, ok } from "../utils/http-response.js";
+import { ZodError } from "zod";
+
+export class DeleteTransactionController {
+  constructor(private readonly deleteTransactionService: DeleteTransactionServiceInterface) {}
+  async execute(req: Request<GetTransactionByIdParamsDTO>, res: Response) {
+    try {
+      const { transactionId } = getTransactionByIdSchema.parse(req.params);
+      const transaction = this.deleteTransactionService.execute(transactionId);
+      if (!transaction) {
+        notFound(res);
+      }
+      return ok(res, transaction);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest(res, error);
+      }
+      return internalServerError(res);
+    }
+  }
+}
