8cc5e91cdd9aa5ab9a67d2b7a708892a103b9195
Adam <adamrpdiass@hotmail.com>
2026-08-15 22:17:02 -0300

    refactor: simplify request and response handling in CreateUserController tests

--- src/controllers/users/create-users.test.ts
+++ src/controllers/users/create-users.test.ts
@@ -6,6 +6,7 @@ import type { CreateUserServiceInterface } from "../../services/interfaces/user/
 import { jest } from "@jest/globals";
 import { faker } from "@faker-js/faker";
 import { EmailAlreadyInUseError } from "../../erros/email.js";
+import { omit } from "../utils/omit.js";
 
 describe("Create User Controller", () => {
   class CreateUserServiceStub implements CreateUserServiceInterface {
@@ -27,21 +28,22 @@ describe("Create User Controller", () => {
     return { createUserService, sut };
   };
 
+  const req = {
+    body: {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 7 }),
+    },
+  } as Request;
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn().mockReturnThis(),
+  } as unknown as Response;
+
   it("should return 201 when creating a user successfully", async () => {
     const { sut } = makeSut();
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
 
     await sut.execute(req, res);
 
@@ -51,192 +53,83 @@ describe("Create User Controller", () => {
       ...req.body,
     });
   });
+
   it("should return 400 if firstName is not provided", async () => {
     const { sut } = makeSut();
 
-    const req = {
-      body: {
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    await sut.execute(req, res);
+    await sut.execute({ ...req.body, firstName: undefined }, res);
 
     expect(res.status).toHaveBeenCalledWith(400);
   });
+
   it("should return 400 if lastName is not provided", async () => {
     const { sut } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    await sut.execute(req, res);
+    await sut.execute({ ...req.body, lastName: undefined }, res);
 
     expect(res.status).toHaveBeenCalledWith(400);
   });
+
   it("should return 400 if email is not provided", async () => {
     const { sut } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    await sut.execute(req, res);
+    await sut.execute({ ...req.body, email: undefined }, res);
 
     expect(res.status).toHaveBeenCalledWith(400);
   });
+
   it("should return 400 if email is not valid", async () => {
     const { sut } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: "invalid_email",
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    await sut.execute(req, res);
+    await sut.execute({ ...req.body, email: "invalid_email" }, res);
 
     expect(res.status).toHaveBeenCalledWith(400);
   });
+
   it("should return 400 if password is not provided", async () => {
     const { sut } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    await sut.execute(req, res);
+    await sut.execute({ ...req.body, password: undefined }, res);
 
     expect(res.status).toHaveBeenCalledWith(400);
   });
+
   it("should return 400 if password is less than 6 characters", async () => {
     const { sut } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({ length: 4 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    await sut.execute(req, res);
-
+    await sut.execute({ ...req.body, password: faker.internet.password({ length: 4 }) }, res);
     expect(res.status).toHaveBeenCalledWith(400);
   });
-  it("should call CreateUserService with correct params", async () => {
-    const { sut } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
+  it("should call CreateUserService with correct params", async () => {
+    const { sut, createUserService } = makeSut();
 
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn().mockReturnThis(),
     } as unknown as Response;
 
-    const executeSpy = jest.spyOn(sut, "execute");
+    const executeSpy = jest.spyOn(createUserService, "execute");
     await sut.execute(req, res);
 
     expect(executeSpy).toHaveBeenCalledWith(req.body);
   });
+
   it("should return 500 if CreateUserService throw", async () => {
-    const { sut } = makeSut();
+    const { sut, createUserService } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    jest.spyOn(sut, "execute").mockImplementationOnce(() => {
+    jest.spyOn(createUserService, "execute").mockImplementationOnce(() => {
       throw new Error();
     });
 
     await sut.execute(req, res);
     expect(res.status).toHaveBeenCalledWith(500);
   });
+
   it("should return 400 if CreateUserService throws EmailAlreadyInUse error", async () => {
-    const { sut } = makeSut();
+    const { sut, createUserService } = makeSut();
 
-    const req = {
-      body: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({ length: 7 }),
-      },
-    } as Request;
-
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as unknown as Response;
-
-    jest.spyOn(sut, "execute").mockImplementationOnce(() => {
+    jest.spyOn(createUserService, "execute").mockImplementationOnce(() => {
       throw new EmailAlreadyInUseError(req.body.email);
     });
 
