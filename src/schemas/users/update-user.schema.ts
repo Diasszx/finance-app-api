368a4aaba687e08c31f6c52f6f13c04ccc1eb693
Adam <adamrpdiass@hotmail.com>
2026-08-17 09:35:21 -0300

    fix: update password minimum length validation in updateUserSchema

--- src/schemas/users/update-user.schema.ts
+++ src/schemas/users/update-user.schema.ts
@@ -9,7 +9,7 @@ export const updateUserSchema = z
       .optional(),
     lastName: z.string().min(2, "O sobrenome deve ter pelo menos 2 caracteres.").trim().optional(),
     email: z.email("O e-mail informado é inválido.").trim().optional(),
-    password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres.").trim().optional(),
+    password: z.string().min(7, "A senha deve ter no mínimo 7 caracteres.").trim().optional(),
   })
   .strict();
 
