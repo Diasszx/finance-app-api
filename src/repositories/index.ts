d7aaaeb32f67cdb5c3476329d2461e6125011c1d
Adam <adamrpdiass@hotmail.com>
2026-09-14 09:41:28 -0300

    refactor: update transaction repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -3,7 +3,9 @@ export * from "./postgres/user/delete-user.js";
 export * from "./postgres/user/get-user-by-id.js";
 export * from "./postgres/user/get-user-by-email.js";
 export * from "./postgres/user/update-user.js";
+export * from "./postgres/user/get-user-balance.js";
 
 export * from "./postgres/transaction/create-transaction.js";
 export * from "./postgres/transaction/get-transaction-by-user-id.js";
 export * from "./postgres/transaction/update-transactions.js";
+export * from "./postgres/transaction/delete-transaction.js";
