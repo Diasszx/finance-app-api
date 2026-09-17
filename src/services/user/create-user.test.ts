d2579fbbc82c57be23a4ceed184f54a49a5c92d8
Adam <adamrpdiass@hotmail.com>
2026-09-17 12:55:19 -0300

    feat: ensure CreateUserService throws if GetUserByEmail throws

--- src/services/user/create-user.test.ts
+++ src/services/user/create-user.test.ts
@@ -113,4 +113,13 @@ describe("Create User Service", () => {
       id: "generated_id",
     });
   });
+
+  it("should throw if GetUserByEmailRepository throws", async () => {
+    const { sut, getUserByEmailRepository } = makeSut();
+    jest.spyOn(getUserByEmailRepository, "execute").mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute(user);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
