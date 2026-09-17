5c6b428c20c810cfba125b23bee281cedabbf429
Adam <adamrpdiass@hotmail.com>
2026-09-17 12:57:42 -0300

    test: ensure CreateUserService throws if idGenerator Throws

--- src/services/user/create-user.test.ts
+++ src/services/user/create-user.test.ts
@@ -122,4 +122,15 @@ describe("Create User Service", () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  it("should throw if IdGeneratorAdapter throws", async () => {
+    const { sut, idGeneratorAdapter } = makeSut();
+    jest.spyOn(idGeneratorAdapter, "execute").mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = sut.execute(user);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
