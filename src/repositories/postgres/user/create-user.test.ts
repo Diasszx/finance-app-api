ce8898d8c1b9b11c3f59aecdc8e49d48dee52455
Adam <adamrpdiass@hotmail.com>
2026-09-22 11:25:38 -0300

    test: ensure PostgresCreateUserRepository creates a user on db

--- src/repositories/postgres/user/create-user.test.ts
+++ src/repositories/postgres/user/create-user.test.ts
@@ -7,6 +7,10 @@ describe("CreateUserRepository", () => {
 
     const result = await sut.execute(user);
 
-    expect(result).not.toBeNull();
+    expect(result.id).toBe(user.id);
+    expect(result.firstName).toBe(user.firstName);
+    expect(result.lastName).toBe(user.lastName);
+    expect(result.email).toBe(user.email);
+    expect(result.password).toBe(user.password);
   });
 });
